9a4b8534787a95d32b77751c9a611a061074af2c
vcerovic <xvcerovic@gmail.com>
2022-07-13 21:38:58 +0200

    Initial commit

---
+++ src/main/java/com/veljkocerovic/timeformeal/TimeForMealApplication.java
@@ -0,0 +1,13 @@
+package com.veljkocerovic.timeformeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeForMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeForMealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veljkocerovic/timeformeal/TimeForMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veljkocerovic.timeformeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeForMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
