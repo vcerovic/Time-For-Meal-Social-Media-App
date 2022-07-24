e35f9d8df7377cbbb8b461720034c3e7d3f9f84a
vcerovic <xvcerovic@gmail.com>
2022-07-24 18:46:01 +0200

    Added basic spring security configs.

---
+++ src/main/java/com/veljkocerovic/timeformeal/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.veljkocerovic.timeformeal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private static final String[] WHITE_LIST_URLS = {"/api/v1/"};
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .antMatchers(WHITE_LIST_URLS).permitAll();
+
+        return http.build();
+    }
+}
