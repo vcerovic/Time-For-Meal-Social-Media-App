bd05f1bfae7e7aeaca7ac4afac6c5898cb469346
vcerovic <xvcerovic@gmail.com>
2022-07-26 17:58:24 +0200

    Created basic recipe controller skeleton.

--- src/main/java/com/veljkocerovic/timeformeal/recipe/RecipeRepository.java
+++ src/main/java/com/veljkocerovic/timeformeal/recipe/RecipeRepository.java
@@ -3,8 +3,11 @@ package com.veljkocerovic.timeformeal.recipe;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
+
+    List<Recipe> findAllByOrderByIdAsc();
     Optional<Recipe> findByName(String name);
 }
